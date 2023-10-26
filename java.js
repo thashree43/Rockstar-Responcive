40695b63d7f622edd352e9e4b96ba909037ee68c
thashree43 <148195520+thashree43@users.noreply.github.com>
2023-10-26 16:36:30 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+// for dropdown
+function toggleDropdown() {
+    var dropdown = document.getElementById("dropdown-content");
+    if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+    } else {
+        dropdown.style.display = "block";
+    }
+}
+
